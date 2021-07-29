672fb9abef6635e67da0e95b85b44d0f37fa0411
unknown <donnymrshd.94@gmail.com>
2021-07-30 00:46:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('image', image);
+    formData.append('name', name);
+    axios.post('http://localhost:3001/post', formData).then((res) => {
+      console.log(res);
+    });
+    console.log(formData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="container">
+        <div class="box">
+          <form onSubmit={handlerSubmit}>
+            <div class="mb-3">
+              <label for="name" class="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                required
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <div class="form-text">We'll never share your name with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <input
+                class="form-control"
+                type="file"
+                id="formFile"
+                required
+                onChange={(e) => {
+                  setImage(e.target.files[0]);
+                }}
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
